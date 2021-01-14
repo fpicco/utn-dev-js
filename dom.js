7c7540ceca25d787182ef98d4868b5a6efec5fe1
fpicco <flor.picco@gmail.com>
2021-01-14 10:33:23 -0300

    cambio de imagen principal

--- dom.js
+++ dom.js
@@ -3,12 +3,28 @@
 
 
 var navSecciones = document.querySelectorAll('.nav__ul a');
-for (i = 0; i < navSecciones.length; ++i) 
+for (var i = 0; i < navSecciones.length; ++i) 
     navSecciones[i].style.color = "red";
 
 var plantasDescripcion = document.querySelectorAll('.plantas__descripcion');
-for (i = 0; i < plantasDescripcion.length; ++i) 
+for (var i = 0; i < plantasDescripcion.length; ++i) 
     plantasDescripcion[i].style.color = "green";
     
-// var imagenVieja = document.getElementById('img-principal');
-// imagenVieja.remove();
+var imagenPrincipal = document.getElementById("imagen-principal");
+imagenPrincipal.removeAttribute("src");
+imagenPrincipal.setAttribute("src", "imagenes/principal2.jpg");
+
+
+//fdfsdfsdfsdfdf-----------------------PROBANDO CON VIDEO DE DORIAN
+
+
+//cambio de colores
+// var rosa = [...document.getElementById("nav"), document.querySelectorAll(".contenedor-I__imagen"), document.querySelectorAll(".plantas__imagen"), document.querySelectorAll(".plantas__descripcion"), document.querySelectorAll("/contacto__container"), document.querySelectorAll(".vermas__contenedor1-imagenes"), document.querySelectorAll(".vermas__contenedor2-imagenes"), document.getElementById(".vermas__fotos-seleccionado"), document.getElementById(".vermas__fotos-seleccionado")];
+
+// for (i = 0; i < rosa.length; i++) 
+//     rosa[i].style.backgroundColor = "red";
+// }
+
+
+
+
