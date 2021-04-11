c8e8607d4a327d421b5180db33229eaa18a86b54
fpicco <flor.picco@gmail.com>
2021-04-11 12:42:31 -0300

    personalizacion de texto segun cant de fotos adj

--- jsFlor/dragdrop.js
+++ jsFlor/dragdrop.js
@@ -8,6 +8,7 @@ function iniciar() {
   });
   soltar.addEventListener("drop", soltado, false);
 }
+
 function soltado(e) {
   e.preventDefault();
   var archivos = e.dataTransfer.files;
@@ -15,12 +16,23 @@ function soltado(e) {
   for (var i = 0; i < archivos.length; i++) {
     lista += i + 1 + ": " + archivos[i].name + "<br>";
   }
-  soltar.innerHTML =
-    "¡Gracias por confiar en nosotros!" +
-    "<br>" +
-    "Las fotos adjuntadas son: " +
-    "<br>" +
-    lista;
+  if (archivos.length == 1) {
+    soltar.innerHTML =
+      "¡Gracias por confiar en nosotros!" +
+      "<br>" +
+      "La foto adjuntada es: " +
+      "<br>" +
+      "<br>" +
+      lista;
+  } else {
+    soltar.innerHTML =
+      "¡Gracias por confiar en nosotros!" +
+      "<br>" +
+      "Las fotos adjuntadas son: " +
+      "<br>" +
+      "<br>" +
+      lista;
+  }
   soltar.style.border = "2px dotted #bf7aff";
   soltar.style.backgroundColor = "white";
   soltar.style.color = "#bf7aff";
